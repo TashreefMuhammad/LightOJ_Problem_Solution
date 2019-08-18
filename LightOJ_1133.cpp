ddcbee5e02e32f7846148be2fef6f19530b50006
Tashreef Muhammad <43475529+TashreefMuhammad@users.noreply.github.com>
2019-08-18 15:16:05 +0600

    Add files via upload

--- LightOJ_1133.cpp
+++ LightOJ_1133.cpp
@@ -1,13 +1,5 @@
-/****
-    * Problem ID        : LightOJ 1133
-    * Problem Name      : Array Simulation
-    * Required Topics   : Brute Force
-****/
-
-//Simple brute force approach
-//Note : Carefully read problem statement. Or you may get presentation error.
-
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -15,41 +7,37 @@ int main(void)
 {
     int t,i,j,k,n,m,b,d,temp;
     char c;
-
     cin>>t;
-    for(i=0;i<t;i++)
+    for(i=0; i<t; i++)
     {
         cin>>n>>m;
         int ar[n];
-
-        for(j=0;j<n;j++)
+        for(j=0; j<n; j++)
             cin>>ar[j];
-
-        for(j=0;j<m;j++)
+        for(j=0; j<m; j++)
         {
             cin>>c;
-
             if(c=='S')
             {
                 scanf("%d",&b);
-                for(k=0;k<n;k++)
+                for(k=0; k<n; k++)
                     ar[k]+=b;
             }
             else if(c=='M')
             {
                 scanf("%d",&b);
-                for(k=0;k<n;k++)
+                for(k=0; k<n; k++)
                     ar[k]*=b;
             }
             else if(c=='D')
             {
                 scanf("%d",&b);
-                for(k=0;k<n;k++)
+                for(k=0; k<n; k++)
                     ar[k]/=b;
             }
             else if(c=='R')
             {
-                for(k=0;k<n/2;k++)
+                for(k=0; k<n/2; k++)
                 {
                     temp=ar[k];
                     ar[k]=ar[n-1-k];
@@ -59,18 +47,19 @@ int main(void)
             else if(c=='P')
             {
                 scanf("%d %d",&b,&d);
-                swap(ar[b],ar[d]);
+                temp=ar[b];
+                ar[b]=ar[d];
+                ar[d]=temp;
+
             }
 
         }
-
         printf("Case %d:\n",i+1);
         printf("%d",ar[0]);
-        for(j=1;j<n;j++)
+        for(j=1; j<n; j++)
             printf(" %d",ar[j]);
         printf("\n");
     }
 
     return 0;
 }
-
